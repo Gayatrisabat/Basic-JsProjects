8c8d5b929415aaf34dabc0d405c8197f03a1fae3
Gayatri Sabat <gayatri.235396208@vcet.edu.in>
2025-12-11 23:27:47 +0530

    Create Dice Roller

---
+++ Random-Number-Generator/calci.js
@@ -0,0 +1,39 @@
+
+//calci and grade checker
+let  num1 = prompt("Enter 1st number");
+let num2 = prompt("Enter 2st number");
+num1 = Number(num1)
+num2 = Number(num2)
+
+remainder = num1 % num2
+ alert(remainder)
+ console.log("remainder is"+" "+  remainder)
+
+division = num1 / num2
+ alert(division)
+ console.log("division is"+ " "+ division)
+product = num2 * num1
+ alert(product)
+ console.log("product is"+ " "+ product)
+
+ difference = num1 - num2
+ alert(difference)
+ console.log("difference is"+" "+  difference)
+
+ sum = num2 + num1
+ alert(sum)
+ console.log("sum is"+ " "+ sum)
+ /*
+ let grade = prompt("Enter your marks out of 100")
+ if (grade >= 75) {
+    alert("soo cool")
+ }
+ else if (75 < grade > 35) {
+    alert("hmm can improve")}
+else if (grade == 35){
+    alert("aareeyy pass hogye tum to guru")
+}
+else  {
+    alert("kyu nahi ho rhi padhai")
+}
+ */
---
+++ Random-Number-Generator/randomNumber.js
@@ -0,0 +1,5 @@
+function randomNumber(1) {
+let number = Math.floor(10*Math.random())
+console.log(number)
+return number ;
+}
