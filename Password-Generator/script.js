b518358425b31e4b8e668df4ae2cc9d25b2d1f87
Gayatri Sabat <gayatri.235396208@vcet.edu.in>
2025-12-15 23:17:59 +0530

    Add Color scheme Selector

---
+++ Password-Generator/script.js
@@ -0,0 +1,16 @@
+let UpperCasechars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let LowerCasechars = "abcdefghijklmnopqrstuvwxyz"
+let Number = "0123456789"
+let symbols = "!@#$%^&*()?':;,"
+
+
+
+
+function generatePassword(length,includeUpperCase,includeLowerCase,includeNumber,includesymbols ){
+return''
+}
+const passwordLength = 8
+let includeUpperCase = true
+let includeLowerCase = true
+let includeNumber = true
+let includesymbols = true
